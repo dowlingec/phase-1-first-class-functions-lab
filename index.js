f4e55f4eaccb27de04033861bc3de20c91b80381
Elizabeth Dowling <dowlinge94@gmail.com>
2022-03-21 12:57:40 -0400

    test 1-2 passing

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // Code your solution in this file!
-// const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
 
-const returnFirstTwoDrivers = (function() = 
-)
+function returnFirstTwoDrivers() {
+    return drivers.slice(0, 2)
+}
+
+function returnLastTwoDrivers() {
+    return drivers.slice(2, 4)
+}
