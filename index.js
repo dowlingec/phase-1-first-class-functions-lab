9195ab9897e6b02ced51d1aa5a844d89c7bbfa4b
Elizabeth Dowling <dowlinge94@gmail.com>
2022-03-22 19:44:30 -0400

    commit tree 43beb7038ea46c054d31f28cd4967bd0257c0f7b

--- index.js
+++ index.js
@@ -9,4 +9,38 @@ function returnLastTwoDrivers() {
     return drivers.slice(2, 4)
 }
 
-const selectingDrivers = [(returnFirstTwoDrivers), (returnLastTwoDrivers)]
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+// study why this happens, array with functions as elements
+
+// function createFareMultiplier() {
+//     const fareDoubler = (fare) => {
+//         return  fare * 3
+//     }
+//     const fareTripler = (fare) => {
+//         return fare * 3
+//     }
+// }
+//faredoubler
+//Declare var using const and assign function 
+
+const fareDoubler = createFareMultiplier(2)
+
+function createFareMultiplier(multiplyBy) {
+    if (multiplyBy === '2') {
+        return function fareDoubler(fare) {
+            return fare * 2
+        }
+    } else if (multiplyBy === '3') {
+        return function fareTripler(fare) {
+            return fare * 3
+        }
+    }
+}
+
+// function selectDifferentDrivers(drivers, fnName) {
+//     if (drivers === returnLastTwoDrivers){
+//         return returnLastTwoDrivers
+//     } else {
+//         return returnFirstTwoDrivers
+//     };
+// }
